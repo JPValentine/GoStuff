854d3220b1bb6889788e9a28ef1ab384e051aa68
JP Valentine <jp.valentine123@gmail.com>
2022-08-11 19:35:15 -0400

    shell added

--- Shell/MyShell.go
+++ Shell/MyShell.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -10,26 +11,35 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	for {
+	for { //infinite loop to constantly be available for new input
 		fmt.Print("> ")
-
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-
-		if err = execInput(input); err != nil {
+		if err = execCommand(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 }
 
-func execInput(input string) error {
+func execCommand(input string) error {
 	input = strings.TrimSuffix(input, "\n")
-
 	args := strings.Split(input, " ")
 
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return errors.New("path required")
+
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
 	cmd := exec.Command(args[0], args[1:]...)
+
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 
---
+++ Shell/shellReadMe.md
@@ -0,0 +1,7 @@
+quick shell program to explore the language a little more
+
+https://simjue.pages.dev/post/2018/07-01-go-unix-shell/
+ran through this blog as a quick guide^
+
+wrote a much more involved shell in college in c++
+but this simple one helped me get aquanted w/ go
