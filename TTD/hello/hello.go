f14da6d8b2c782912fb763b5b647339ddd620b9c
JP Valentine <jp.valentine123@gmail.com>
2022-08-13 09:15:46 -0400

    learning by refactoring

--- TTD/hello/hello.go
+++ TTD/hello/hello.go
@@ -4,10 +4,25 @@ import "fmt"
 
 //main func
 func main() {
-	fmt.Println(hello("world"))
+	fmt.Println(hello("world", ""))
 }
 
+const englishHelloPrefix = "hello "
+const spanishHelloPrefix = "hola "
+const frenchHelloPrefix = "bonjour "
+const spanish = "spanish"
+const french = "french"
+
 //simple example of a unit test
-func hello(name string) string {
-	return "hello " + name
+func hello(name string, lang string) string {
+	if name == "" {
+		name = "world"
+	}
+	if lang == spanish {
+		return spanishHelloPrefix + name
+	}
+	if lang == french {
+		return frenchHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
--- TTD/hello/hello_test.go
+++ TTD/hello/hello_test.go
@@ -2,10 +2,31 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := hello("chris")
-	want := "hello chris"
+func testhello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("chris", "")
+		want := "hello chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := hello("", "")
+		want := "hello world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := hello("elodie", "spanish")
+		want := "hola elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := hello("name", "french")
+		want := "bonjour name"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
