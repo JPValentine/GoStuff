06d829bda293be29b504616352367617d430a461
JP Valentine <jp.valentine123@gmail.com>
2022-09-02 17:07:47 -0400

    HTTP Server

--- HTTPserver/server.go
+++ HTTPserver/server.go
@@ -1,5 +1,32 @@
 package main
 
-func ListenAndServe(addr string, handler Handler) error{
+import(
+	"net/http"
+	"fmt"
+	"strings"
+)
 
+type PlayerStore interface{
+	GetPlayerScore(name string) int
 }
+
+type PlayerServer struct{
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request)(){
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string{
+	if name == "Pepper"{
+		return "20"
+	}
+	if name == "Bob"{
+		return "10"
+	}
+	return ""
+}//GetPlayerScore
+
+
--- HTTPserver/server_test.go
+++ HTTPserver/server_test.go
@@ -1,1 +1,57 @@
-package main;
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubPlayerStore struct{
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func TestGETPlayers(t *testing.T){
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper":20,
+			"Bob":10,
+		},
+	}
+
+	server := &PlayerServer{&store}
+
+	t.Run("returns Pepper's score", func(t *testing.T){
+		request :=newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertResponseBody(t,response.Body.String(),"20")
+	})
+	t.Run("returns bob's score", func(t *testing.T){
+		request :=newGetScoreRequest("Bob")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertResponseBody(t,response.Body.String(),"10")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request{
+	req,_ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name),nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string){
+	t.Helper()
+	if got!= want{
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
+}
