cd98231ed721c8490153dd029de05adf3f645e1a
JPValentine <jp.valentine123@gmail.com>
2022-09-11 08:45:38 -0400

    time

--- exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
+++ exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -2,29 +2,37 @@ package booking
 
 import "time"
 
-// Schedule returns a time.Time from a string containing a date.
+// Schedule returns a time.Time from a string containing a date
 func Schedule(date string) time.Time {
-	const form = "7/13/2020 20:32:00"
-	t,_=time.Parse(form, date)
+	t, _ := time.Parse("1/2/2006 15:04:05", date)
 	return t
 }
 
-// HasPassed returns whether a date has passed.
+// HasPassed returns whether a date has passed
 func HasPassed(date string) bool {
-	panic("Please implement the HasPassed function")
+	t, _ := time.Parse("January 2, 2006 15:04:05", date)
+	return t.Before(time.Now())
 }
+         
 
-// IsAfternoonAppointment returns whether a time is in the afternoon.
+// IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	t, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
+	return t.Hour() >= 12 && t.Hour() < 18
 }
 
-// Description returns a formatted string of the appointment time.
+// Description returns a formatted string of the appointment time
 func Description(date string) string {
-	panic("Please implement the Description function")
+	t, _ := time.Parse("1/2/2006 15:04:05", date)
+	return "You have an appointment on " + t.Format("Monday, January 2, 2006, at 15:04.")
 }
 
-// AnniversaryDate returns a Time with this year's anniversary.
+// AnniversaryDate returns a Time with this year's anniversary
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+	currentYear := time.Now().Year()
+	return time.Date(currentYear, time.September, 15, 0, 0, 0, 0, time.UTC)
 }
+
+        
+
+          
