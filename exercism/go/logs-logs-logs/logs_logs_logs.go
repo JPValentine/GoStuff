f2c4845bb296c496fe2a34fb513f08ab232f6224
JP Valentine <jp.valentine123@gmail.com>
2022-09-13 15:45:35 -0400

    beat the runes

--- exercism/go/logs-logs-logs/logs_logs_logs.go
+++ exercism/go/logs-logs-logs/logs_logs_logs.go
@@ -2,19 +2,19 @@ package logs
 
 import(
 	"fmt"
+	"strings"
 )
 
 // Application identifies the application emitting the given log.
 func Application(log string) string {
 	for _,char := range log{
-		x:=fmt.Sprintf("%U",char)
-		if x == "U+2757"{
+		if fmt.Sprintf("%U",char) == "U+2757"{
 			return "recommendation"
 		}
-		if x == "U+1F50D"{
+		if fmt.Sprintf("%U",char) == "U+1F50D"{
 			return "search"
 		}
-		if x == "U+2600"{
+		if fmt.Sprintf("%U",char) == "U+2600"{
 			return "weather"
 		}
 	}
@@ -24,19 +24,13 @@ func Application(log string) string {
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	y:=fmt.Sprintf("%U",oldRune)
-	myStr:=""
-	for i,c := range log{
-		x:=fmt.Sprintf("%U", c)
-		if x == y{
-			log[i] = newRune
-		}
-	}
-	return log
+	newStr:=fmt.Sprintf("%c",newRune)
+	oldStr:=fmt.Sprintf("%c",oldRune)
+	return strings.Replace(log,oldStr,newStr,-1)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return len([]rune(log))<=limit
 }
