191ca854663d21cab4ab5155d648436384232b8f
JP Valentine <jp.valentine123@gmail.com>
2022-09-14 16:06:57 -0400

    errors

--- exercism/go/the-farm/the_farm.go
+++ exercism/go/the-farm/the_farm.go
@@ -1,10 +1,33 @@
 package thefarm
 
-// See types.go for the types defined for this exercise.
+import (
+	"errors"
+	"fmt"
+)
 
-// TODO: Define the SillyNephewError type here.
+// See types.go for the types defined for this exercise.
+var ErrNegative = errors.New("negative fodder")
+var ErrDivZero = errors.New("division by zero")
+
+func SillyNephewError(cows int) error {
+	x := errors.New(fmt.Sprintf("silly nephew, there cannot be %v cows", cows))
+	return x
+}
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	panic("Please implement DivideFood")
+	floatAmount, err := weightFodder.FodderAmount()
+	if err == ErrScaleMalfunction && floatAmount >= 0.0 {
+		out := (floatAmount * 2) / float64(cows)
+		return out, nil
+	} else if (err == ErrScaleMalfunction || err == nil) && floatAmount < 0.0 {
+		return 0.0, ErrNegative
+	} else if cows == 0 {
+		return 0.0, ErrDivZero
+	} else if cows < 0 {
+		return 0.0, SillyNephewError(cows)
+	} else if err != nil {
+		return 0.0, err
+	} 
+	return floatAmount / float64(cows), err
 }
